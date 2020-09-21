440f74940962dd41c6199f544c7694af94f0647a
ChrisBAshton <christopher.ashton@digital.cabinet-office.gov.uk>
2020-09-21 14:52:23 +0100

    WIP: add 'last updated' banner
    I did initially try to do this in tech-docs-gem itself, which
    involves adding a new property (e.g. 'show_last_updated_banner')
    to the config in govuk-developer-docs, and looking for the
    property in tech-docs-gem.
    A couple of issues with that approach:
    
    1. I have to figure out how to inject frontmatter variables
       directly, as we can't put the last_updated value in the
       YML itself. Using 'proxy' doesn't seem appropriate here
       as that is intended for dynamic _paths_, not pages whose
       content we know ahead of time.
    2. Breaking change (unless introduced carefully).
    
    Next steps:
    - figure out how to pass different values for different docs
      (maybe try to receive 'last_reviewed_on' value for now)
    - figure out how to retrieve commit date per file

---
+++ helpers/last_updated_helpers.rb
@@ -0,0 +1,6 @@
+module LastUpdatedHelpers
+  def last_updated(current_page)
+    puts current_page # TODO - retrieve last updated date from latest commit for that file
+    "1st March 2020"
+  end
+end
